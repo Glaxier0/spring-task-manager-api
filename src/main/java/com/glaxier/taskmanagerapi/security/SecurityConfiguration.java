add3d22e79b6d7484a48b44f25b4a74f224d6b14
glaxier0 <tahir2001@hotmail.com>
2021-12-15 22:00:10 +0300

    Added part of jwt auth

--- src/main/java/com/glaxier/taskmanagerapi/security/SecurityConfiguration.java
+++ src/main/java/com/glaxier/taskmanagerapi/security/SecurityConfiguration.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+
 @EnableWebSecurity
 class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
@@ -25,5 +27,7 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .httpBasic();
     }
 
-    // ...
+    @Bean public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
