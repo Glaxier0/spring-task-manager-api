16bcf3b816bb9d6689c08ef5bda6035ee9e3a249
glaxier0 <tahir2001@hotmail.com>
2021-12-20 03:00:58 +0300

    jwt auth v2

---
+++ src/main/java/com/glaxier/taskmanagerapi/service/UserDetailsImpl.java
@@ -0,0 +1,96 @@
+package com.glaxier.taskmanagerapi.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.glaxier.taskmanagerapi.model.Users;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private String id;
+
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+    private Integer age;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserDetailsImpl build(Users user) {
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getAge(),
+                new ArrayList<>());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        UserDetailsImpl user = (UserDetailsImpl) object;
+        return Objects.equals(id, user.id);
+    }
+}
---
+++ src/main/java/com/glaxier/taskmanagerapi/service/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.glaxier.taskmanagerapi.service;
+
+import com.glaxier.taskmanagerapi.model.Users;
+import com.glaxier.taskmanagerapi.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Users user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Invalid username or password."));
+        return UserDetailsImpl.build(user);
+    }
+}
