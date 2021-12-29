6c01e302ce2b0ca637a653a268e6526f2c5e0b7a
glaxier0 <tahir2001@hotmail.com>
2021-12-29 22:14:19 +0300

    Custom error handling done

---
+++ src/main/java/com/glaxier/taskmanagerapi/Util/ImageResizer.java
@@ -0,0 +1,32 @@
+package com.glaxier.taskmanagerapi.Util;
+
+import net.coobird.thumbnailator.Thumbnails;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+@Component
+public class ImageResizer {
+
+    @Value("${image.width}")
+    private int IMAGE_WIDTH;
+    @Value("${image.height}")
+    private int IMAGE_HEIGHT;
+
+    public String resizeImage(MultipartFile avatar) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        Thumbnails.of(avatar.getInputStream()).size(IMAGE_WIDTH, IMAGE_HEIGHT)
+                .outputFormat("JPEG")
+                .outputQuality(1)
+                .toOutputStream(outputStream);
+
+        byte[] imageByteArray = Base64.encodeBase64(outputStream.toByteArray());
+
+        return new String(imageByteArray);
+    }
+}
